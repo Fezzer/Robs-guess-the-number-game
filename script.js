973ae2e11f96b135462552cc33e8f4a768fb8558
Rob S <me@rob-saunders.co.uk>
2022-12-18 23:14:35 +0000

    Rearranged the logic to check for round number and number of guesses first

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+  CB
+    - A general comment, concentrate on the formatting of your code. The first thing I did was to format your code so that I could read it more easily!
+    - Here are a few suggestions:
+      - Tabbing should be consistent. This will help you visualise nested code more easily,
+      - Don't use multiple blank lines.
+      - Break long lines of code.
+      - Multiline blocks of code, i.e. if(...){...}, for(...){...}, etc., should have an empty line before and after them.
+*/
+
 // Guess the computer's number game
 
 // Rules
@@ -17,76 +27,147 @@
 var wins = 0;
 var losses = 0;
 
-
-alert(`Hello and welcome to 2022's most loved game. \n You are about to face-off with the most feared opponent that ever existed; that's right, one of my in-laws. Oh, sorry, dad jokes accepted! You will face off with a multi-cored, silcon-based, Vulcan inspired AI. Are you ready? \n Here are the instructions. The AI will pick a number at random and you have to guess it. You guess it; you win. You guess wrong; you lose the round. Simple! You can choose how hard to make it by increasing or reducing the range the AI can choose from. A lower range increases your odds of guessing right, but increasing the range will drastically reduce your chances of winning. \n Rules: \n 1. You must enter name \n 2. You must enter a number of rounds \n 3. You must set the range your opponent can choose from. \n Want to give it a go?`);
-
+/* 
+  CB
+  - Split up the alert message string up to make it more readable.
+  - Removed whitespace around the '\n' as this will be visible in the displayed alert message.
+  - Changed backtick string delimiters to double quotes. You are not doing any string interpolation here so you do not need
+    the functionality associated with the backtick delimiter.
+  - Corrected a typo in 'silicon-based'. 
+*/
+alert(
+  "Hello and welcome to 2022's most loved game.\n" +
+  "You are about to face-off with the most feared opponent that ever existed; that's right, one of my in-laws. " +
+  "Oh, sorry, dad jokes accepted! You will face off with a multi-cored, silicon-based, Vulcan inspired AI. Are you ready?\n" +
+  "Here are the instructions. The AI will pick a number at random and you have to guess it. You guess it; you win. " +
+  "You guess wrong; you lose the round. Simple! You can choose how hard to make it by increasing or reducing the range " +
+  "the AI can choose from. A lower range increases your odds of guessing right, but increasing the range will drastically reduce your chances of winning.\n" +
+  "Rules:\n" +
+  "1. You must enter name\n" +
+  "2. You must enter a number of rounds\n" +
+  "3. You must set the range your opponent can choose from.\n" +
+  "Want to give it a go?"
+);
 
 //Enter username
 var userName = prompt("Please enter your name");
-    console.log("Hi " + userName + ", You'll never beat me!");
-    /*if(userName === null) {
-        break;
-    }*/
+// CB - Use string interpolation here.
 
-    //Choose number of rounds
-var numberOfRounds = parseInt(prompt(`'${userName}, now choose the number of rounds you want to play.'`));
+console.log(
+    `Hi ${userName}, You'll never beat me!`
+);
 
 
-    if(numberOfRounds < 5) {
-            console.log(`'${numberOfRounds}?!! Wow, weak!'`);
+//Choose number of rounds
+// CB - Do you want the single quotes in all these messages? - Only in the Console - RS- thank you
+var numberOfRounds = parseInt(prompt(`${userName}, now choose the number of rounds you want to play.`));
 
-        } else if(numberOfRounds >= 5 && numberOfRounds < 10) {
-            console.log(`'Ok, ${numberOfRounds} is not bad!'`);
+if(numberOfRounds < 5) {
 
-        } else {
-            (alert(`'${numberOfRounds}?! Wow, you're in for a mega challenge!!!'`));
-        }
+  console.log(
+    `'${numberOfRounds}?!! Wow, weak!'`
+    );
 
-        console.log("'<initiating warm up procedure>'");
+} else if(numberOfRounds >= 5 && numberOfRounds < 10) {
+
+  console.log(
+    `'Ok, ${numberOfRounds} is not bad!'`
+    );
+
+} else {
+
+  (alert(
+    `${numberOfRounds}?! Wow, you're in for a mega challenge!!!`
+    ));
+
+}
+
+console.log("'<initiating warm up procedure>'");
 
 // Choose the computer's selection range
 var range = parseInt(prompt(`${userName}, choose the highest number the computer can choose from.`));
 
-        console.log(`'Understood! Between 0 and ${range}. Choosing in 5,'`);
-            for (var i = 4; i > 0; i--) {
-                console.log(i);
-            };
+console.log(
+    `'Understood! Between 0 and ${range}. Choosing in 5,'`
+    );
+
+for (var i = 4; i > 0; i--) {
+  console.log(i);
+};
 
 // Computer chooses a random number based on user selection
-var guesses = 0;
+
+var roundNumber = 0;
 do {
-var roundNumber = 1;
+  // CB - This variable is only ever written to, it's value is never used. It can be removed. RS -Thank you
 
-var computerNumber = Math.floor(Math.random() * range);
+  if(roundNumber == numberOfRounds){
+    break;
+  }
+  confirm(`Ready for Round ${roundNumber+1}`)
+  var computerNumber = Math.floor(Math.random() * range);
 
-    console.log(`You'll never guess it weak human! I chose ${computerNumber}`);
+  // Do you want to show the user the computers guess before they have guessed? - RS - For the purpose of testing, yes as it is a quick win if need be
+  console.log(
+    `You'll never guess it weak human! I chose ${computerNumber}`
+    );
 
-    do {
-        var userGuess = parseInt(prompt(`Ok ${userName}, time to enter guess number ${guesses +1}`));
+    //  CB - As you have a fixed number of guesses a for(var guesses = 0; guesses < 5; guesses++) loop would be better here. That would make your previously declared guesses variable redundant.
+ 
+  for (var guesses = 0; guesses < 5; guesses++) {
 
-            if(userGuess != computerNumber) {
-                guesses++;
-                confirm(`Try again`);
-                continue;
+    if(guesses == 4) {
 
-            } else if (guesses === 4) {
-                losses++;
-                roundNumber++;
-                alert(`Too many guesses \n The score is ${wins} - ${losses}`);
-            };
+      losses++;
+      roundNumber++;
+      alert(`Too many guesses \n The score is ${wins} - ${losses}`);
+      break;
 
-            wins++; 
-            alert(`Wow!! ${userGuess} is correct! \n The score is ${wins} - ${losses}`);
+    } else {
+
+      var userGuess = parseInt(prompt(`Ok ${userName}, time to enter guess number ${guesses +1}`));
+
+      /*
+      CB - The logic flow in here needs to be:
+          - Check and then record the result of the guess.
+          - If correct, record win and continue on to the next round. The number of guesses don't matter here.
+          - If wrong, record loss and then check guess count.
+              - If guesses less than allowed number, let the user have another guess.
+              - If guesses greater than allowed number, move on to next round.
+      */
+        if(userGuess == computerNumber) {
+
+            wins++;
             roundNumber++;
-            guesses= 0;
+            alert(`Wow!! ${userGuess} is correct! \n The score is ${wins} - ${losses}`);
             break;
-           
 
-    } while (true);
+        } else {
+
+          confirm(`Try again. You have ${4-guesses} guesses left.`);
+
+        };
+
+    }
+  }
+  }while (wins + losses < numberOfRounds);
 
 
+  // CB - You probably want to show the final score after all the rounds have been completed.
+  
+  if(wins == losses) {
 
+    console.log("We are equal worthy foe!!!");
+    alert(`The final score is ${wins} - ${losses}. YOU Drew`);
 
-console.log(`The final score is ${wins} - ${losses}`);
+  } else if(wins > losses) {
 
-} while (wins + losses < numberOfRounds);
+    console.log("NOOOOOOOOOOOOOOOO my computer chips.....are......MELTING!!!!!");
+    alert(`The final score is ${wins} - ${losses}. YOU WIN!!!!!!!!!!`);
+
+  } else {
+
+    console.log("MWUH HAHAHAHAHAHA YOU LOOOOOOSER!!! Your puny little organic processor is no match for my silicon processor built by beings with an organic proces.....wait?! I WIN, YOU SUCK!!!");
+    alert(`The final score is ${wins} - ${losses}. Sorry, but you lost this game.`);
+
+  };
