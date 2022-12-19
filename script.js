6db11eabee84de25436b3f32d81bb42303a0af4d
Fezzer <chris.bowles@thescreamingsheep.net>
2022-12-19 10:51:41 +0000

    refactor script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-/*
-  CB
-    - A general comment, concentrate on the formatting of your code. The first thing I did was to format your code so that I could read it more easily!
-    - Here are a few suggestions:
-      - Tabbing should be consistent. This will help you visualise nested code more easily,
-      - Don't use multiple blank lines.
-      - Break long lines of code.
-      - Multiline blocks of code, i.e. if(...){...}, for(...){...}, etc., should have an empty line before and after them.
-*/
-
 // Guess the computer's number game
 
 // Rules
@@ -27,14 +17,6 @@
 var wins = 0;
 var losses = 0;
 
-/* 
-  CB
-  - Split up the alert message string up to make it more readable.
-  - Removed whitespace around the '\n' as this will be visible in the displayed alert message.
-  - Changed backtick string delimiters to double quotes. You are not doing any string interpolation here so you do not need
-    the functionality associated with the backtick delimiter.
-  - Corrected a typo in 'silicon-based'. 
-*/
 alert(
   "Hello and welcome to 2022's most loved game.\n" +
   "You are about to face-off with the most feared opponent that ever existed; that's right, one of my in-laws. " +
@@ -51,7 +33,6 @@ alert(
 
 //Enter username
 var userName = prompt("Please enter your name");
-// CB - Use string interpolation here.
 
 console.log(
     `<ROBOT> "Hi ${userName}, You'll never beat me!"`
@@ -59,27 +40,15 @@ console.log(
 
 
 //Choose number of rounds
-// CB - Do you want the single quotes in all these messages? - Only in the Console - RS- thank you
 var numberOfRounds = parseInt(prompt(`${userName}, now choose the number of rounds you want to play.`));
 
+// CB - Moved to single line statements and remove redundant brackets.
 if(numberOfRounds < 5) {
-
-  console.log(
-    `<ROBOT> "${numberOfRounds}?!! Wow, weak!"`
-    );
-
-} else if(numberOfRounds >= 5 && numberOfRounds < 10) {
-
-  console.log(
-    `<ROBOT> "Ok, ${numberOfRounds} is not bad!"`
-    );
-
+  console.log(`<ROBOT> "${numberOfRounds}?!! Wow, weak!"`);
+} else if(numberOfRounds < 10) { // CB - You don't need the 'numberOfRound <= 5' here are that has to be true for it to get to this statement.
+  console.log(`<ROBOT> "Ok, ${numberOfRounds} is not bad!"`);
 } else {
-
-  (alert(
-    `${numberOfRounds}?! Wow, you're in for a mega challenge!!!`
-    ));
-
+  alert(`${numberOfRounds}?! Wow, you're in for a mega challenge!!!`);
 }
 
 console.log(`<ROBOT> "<initiating warm up procedure>"`);
@@ -87,90 +56,71 @@ console.log(`<ROBOT> "<initiating warm up procedure>"`);
 // Choose the computer's selection range
 var range = parseInt(prompt(`${userName}, choose the highest number the computer can choose from.`));
 
-console.log(
-    `<ROBOT> "Understood! Between 0 and ${range}. Choosing in 5,"`
-    );
+console.log(`<ROBOT> "Understood! Between 0 and ${range}. Choosing in 5,"`);
 
 for (var i = 4; i > 0; i--) {
   console.log(i);
 };
 
 // Computer chooses a random number based on user selection
+var numberOfGuesses = 5; // CB - Make this a variable so it can be easily changed in the future. Try and keep a mind to what might change.
 
-var roundNumber = 0;
-do {
-  // CB - This variable is only ever written to, it's value is never used. It can be removed. RS -Thank you
-
-  if(roundNumber == numberOfRounds){
-    break;
-  }
-  confirm(`Ready for Round ${roundNumber+1}`)
+// CB - We know the number of rounds so use a 'for' loop.
+//    - I edited the loop variable name to match the same syntax as the inner loop. 'round' and 'guess'.
+for (let round = 0; round < numberOfRounds; round++) {
+  confirm(`Ready for Round ${round + 1}`)
   var computerNumber = Math.floor(Math.random() * range);
 
-  // Do you want to show the user the computers guess before they have guessed? - RS - For the purpose of testing, yes as it is a quick win if need be
-  console.log(
-    `<ROBOT> "You'll never guess it weak human! I chose ${computerNumber}"`
-    );
+  console.log(`<ROBOT> "You'll never guess it weak human! I chose ${computerNumber}"`);
+  
+  let isWin = false;
 
-    //  CB - As you have a fixed number of guesses a for(var guesses = 0; guesses < 5; guesses++) loop would be better here. That would make your previously declared guesses variable redundant.
- 
-  for (var guesses = 0; guesses < 6; guesses++) {
+  for (var guess = 0; guess < numberOfGuesses; guess++) {
+    if (guess > 0)
+    {
+      let remainingGuesses = numberOfGuesses - guess;
+      confirm(`Try again. You have ${remainingGuesses} guesses left.`);
+      console.log(`<ROBOT> "${userGuess} ?!?! No chance!!! ${remainingGuesses} guesses left? You have no chance"`);
+    }
 
-    if(guesses == 5) {
+    var userGuess = parseInt(prompt(`Ok ${userName}, time to enter guess number ${guess + 1}`));
 
-      losses++;
-      roundNumber++;
-      alert(`Too many guesses \n The score is ${wins} - ${losses}`);
-      console.log(`<ROBOT> "YOU SUCK"`);
+    if(userGuess == computerNumber) {
+      isWin = true;
       break;
-
-    } else {
-
-      var userGuess = parseInt(prompt(`Ok ${userName}, time to enter guess number ${guesses +1}`));
-
-      /*
-      CB - The logic flow in here needs to be:
-          - Check and then record the result of the guess.
-          - If correct, record win and continue on to the next round. The number of guesses don't matter here.
-          - If wrong, record loss and then check guess count.
-              - If guesses less than allowed number, let the user have another guess.
-              - If guesses greater than allowed number, move on to next round.
-      */
-        if(userGuess == computerNumber) {
-
-            wins++;
-            roundNumber++;
-            alert(`Wow!! ${userGuess} is correct! \n The score is ${wins} - ${losses}`);
-            console.log(`<ROBOT> "Uncomputing...user win...not recognised"`);
-            break;
-
-        } else {
-
-          confirm(`Try again. You have ${4-guesses} guesses left.`);
-          console.log(`<ROBOT> "${userGuess} ?!?! No chance!!! ${4-guesses} guesses left? You have no chance"`);
-
-        };
-
     }
   }
-  }while (wins + losses < numberOfRounds);
 
+  let resultText = "";
+  let logText = "";
 
-  // CB - You probably want to show the final score after all the rounds have been completed.
-  
-  if(wins == losses) {
-
-    console.log(`<ROBOT> "We are equal worthy foe!!!"`);
-    alert(`The final score is ${wins} - ${losses}. YOU Drew`);
-
-  } else if(wins > losses) {
-
-    console.log(`<ROBOT> "NOOOOOOOOOOOOOOOO my computer chips.....are......MELTING!!!!!"`);
-    alert(`The final score is ${wins} - ${losses}. YOU WIN!!!!!!!!!!`);
-
+  if (isWin) {
+    wins++;
+    resultText = `Wow!! ${userGuess} is correct!`;
+    logText = "<ROBOT> \"Uncomputing...user win...not recognised\"";
   } else {
+    losses++;
+    resultText = "Too many guesses";
+    logText = "<ROBOT> \"YOU SUCK\"";
+  }
 
-    console.log(`<ROBOT> "MWUH HAHAHAHAHAHA YOU LOOOOOOSER!!! Your puny little organic processor is no match for my silicon processor built by beings with an organic proces.....wait?! I WIN, YOU SUCK!!!"`);
-    alert(`The final score is ${wins} - ${losses}. Sorry, but you lost this game.`);
+  alert(`${resultText} \n The score is ${wins} - ${losses}`);
+  console.log(logText);
+}
 
-  };
+// CB - A lesson in DRY (Don't Repeat Yourself). Look for repeated code and try to reduce it.
+//    - I did the same just above.
+let resultText = "";
+
+if(wins == losses) {
+  console.log(`<ROBOT> "We are equal worthy foe!!!"`);
+  resultText = "YOU Drew";
+} else if(wins > losses) {
+  console.log(`<ROBOT> "NOOOOOOOOOOOOOOOO my computer chips.....are......MELTING!!!!!"`);
+  resultText = "YOU WIN!!!!!!!!!!";
+} else {
+  console.log(`<ROBOT> "MWUH HAHAHAHAHAHA YOU LOOOOOOSER!!! Your puny little organic processor is no match for my silicon processor built by beings with an organic proces.....wait?! I WIN, YOU SUCK!!!"`);
+  resultText = "Sorry, but you lost this game.";
+};
+
+alert(`The final score is ${wins} - ${losses}. ${resultText}.`);
